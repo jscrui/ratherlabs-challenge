e8d08b1181ac9427a9d20f69869d7e9dabc03aeb
Juan Snaider Cruickshank <32991293+jscrui@users.noreply.github.com>
2023-02-10 22:42:14 -0300

    Updates on interfaces to expand access

--- contracts/@sushiswap/contracts/interfaces/IMasterChef.sol
+++ contracts/@sushiswap/contracts/interfaces/IMasterChef.sol
@@ -22,5 +22,5 @@ interface IMasterChef {
     function pendingSushi(uint256 _pid, address _user) external view returns (uint256);
     function deposit(uint256 _pid, uint256 _amount) external;
     function withdraw(uint256 _pid, uint256 _amount) external;    
-    function emergencyWithdraw(uint256 pid, address to) external;
+    function emergencyWithdraw(uint256 pid) external;
 }
