de5e0b7a9fb6d615972d26cd8fa408267becfd7b
Juan Snaider Cruickshank <32991293+jscrui@users.noreply.github.com>
2023-02-11 15:02:12 -0300

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,8 +6,27 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
+const sushiRouter = "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F";
+const masterChef = "0xc2EdaD668740f1aA35E4D8f227fB8E17dcA888Cd";
+const masterChefV2 = "0xEF0881eC094552b2e128Cf945EF17a6752B4Ec5d";
+
 async function main() {
+
+  const [deployer] = await ethers.getSigners();
   
+  console.log("Deploying contracts with the account:", deployer.address);
+
+    //Deploy SushiWallet
+    const sushiWallet = await ethers.getContractFactory("SushiWallet");
+    SushiWallet = await sushiWallet.deploy(sushiRouter, masterChef, masterChefV2);
+    await SushiWallet.deployed();
+
+    console.log("SushiWallet deployed to:", SushiWallet.address);
+
+    //Show deployment gas cost
+    const tx = await deployer.getTransactionReceipt(SushiWallet.deployTransaction.hash);
+    console.log("Gas used:", tx.gasUsed.toString());
+    
 }
 
 // We recommend this pattern to be able to use async/await everywhere
