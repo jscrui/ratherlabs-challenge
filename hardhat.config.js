8d432e3a674b7941844a802a7657bf64c03243e2
Juan Snaider Cruickshank <32991293+jscrui@users.noreply.github.com>
2023-02-10 22:43:58 -0300

    Hardhat config and packages updates

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,25 @@
 require("@nomicfoundation/hardhat-chai-matchers");
+require("hardhat-gas-reporter");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+        optimizer: {
+            enabled: true,
+            runs: 200
+        }
+    }
+},
+  gasReporter: {
+    enabled: true
+},
   networks: {
     hardhat: {
       forking: {
         url: "https://eth-mainnet.g.alchemy.com/v2/D3PM3hdkUDmlaYQp7UJXw0nbpWO4ufCh",
-        blockNumber: 16588388,
-        gas: 12000000,
-        gasPrice: 100000000000 
+        blockNumber: 16588388,        
       },
     },
   }
