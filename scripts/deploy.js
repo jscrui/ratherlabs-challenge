1f37fcfccdb13a32a111737b1b288f11d66b32db
Juan Snaider Cruickshank <32991293+jscrui@users.noreply.github.com>
2023-02-16 19:50:11 -0300

    Update deploy.js
    Fix deploy bug.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,3 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
-
 const sushiRouter = "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F";
 const masterChef = "0xc2EdaD668740f1aA35E4D8f227fB8E17dcA888Cd";
 const masterChefV2 = "0xEF0881eC094552b2e128Cf945EF17a6752B4Ec5d";
@@ -18,14 +10,11 @@ async function main() {
 
     //Deploy SushiWallet
     const sushiWallet = await ethers.getContractFactory("SushiWallet");
-    SushiWallet = await sushiWallet.deploy(sushiRouter, masterChef, masterChefV2);
+    const SushiWallet = await sushiWallet.deploy(sushiRouter, masterChef, masterChefV2);
     await SushiWallet.deployed();
 
-    console.log("SushiWallet deployed to:", SushiWallet.address);
+    console.log("SushiWallet deployed to:", SushiWallet.address);    
 
-    //Show deployment gas cost
-    const tx = await deployer.getTransactionReceipt(SushiWallet.deployTransaction.hash);
-    console.log("Gas used:", tx.gasUsed.toString());
     
 }
 
